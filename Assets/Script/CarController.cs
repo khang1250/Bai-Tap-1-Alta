bb2ccdc08eac729c96c8b1f76aa404fbd015d832
khang1250 <65101024+khang1250@users.noreply.github.com>
2022-02-24 10:55:31 +0700

    Update
    add trail, air control

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -17,13 +17,22 @@ public class CarController : MonoBehaviour
 
     private bool grounded;
 
-    public Transform groundRay;
+    public Transform groundRay, groundRay2;
     public LayerMask isGround;
     public float groundRayLength = 2.25f;
+
+    private float dragOnGround;
+    public float gravity = 10f;
+    
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 25f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb.transform.parent = null;
+
+        dragOnGround = rb.drag;
     }
 
     // Update is called once per frame
@@ -47,6 +56,14 @@ public class CarController : MonoBehaviour
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Mathf.Sign(speedInput) * (rb.velocity.magnitude / maxSpeed), 0f)); 
         }
 
+
+
+        //turning the wheel
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
+
+
+
         transform.position = rb.position;
     }
 
@@ -55,10 +72,26 @@ public class CarController : MonoBehaviour
         grounded = false;
 
         RaycastHit hit;
+        Vector3 normalTarget = Vector3.zero;
 
         if(Physics.Raycast(groundRay.position, -transform.up, out hit, groundRayLength, isGround))
         {
             grounded = true; 
+
+            normalTarget = hit.normal;
+        }
+
+        if(Physics.Raycast(groundRay2.position, -transform.up, out hit, groundRayLength, isGround))
+        {
+            grounded = true;
+
+            normalTarget = (normalTarget + hit.normal) / 2f;
+        }
+
+        //when on ground rotate to match normal
+        if (grounded)
+        {
+            transform.rotation = Quaternion.FromToRotation(transform.up, normalTarget) *  transform.rotation;
         }
 
 
@@ -66,6 +99,14 @@ public class CarController : MonoBehaviour
         if (grounded)
         {
             rb.AddForce(transform.forward * speedInput * 1000f);
+
+            rb.drag = dragOnGround;
+        }
+        else
+        {
+            rb.drag = .1f;
+
+            rb.AddForce(Vector3.up * -gravity * 100f);
         }
 
         if(rb.velocity.magnitude > maxSpeed)
